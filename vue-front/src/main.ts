d36324759f97661247741a0230c7734f49d1d078
Sullivanml299 <70675302+Sullivanml299@users.noreply.github.com>
2020-11-19 01:11:53 -0500

    Setting up BootstrapVue

--- vue-front/src/main.ts
+++ vue-front/src/main.ts
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import BootstrapVue from 'bootstrap-vue' //added by me
+//src/main.js
+import 'bootstrap/dist/css/bootstrap.css' //added by me
+import 'bootstrap-vue/dist/bootstrap-vue.css' //added by me
 
+Vue.use(BootstrapVue) //added by me
 Vue.config.productionTip = false
 
 new Vue({
